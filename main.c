38321ea4fafeb22a54ef8be4bba0e379a5194104
gabrielgermanoo <gabriel.lucasgermano@gmail.com>
2022-06-17 13:43:26 -0300

    esqueci a main

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "compress/compress.h"
+#include "decompress/decompress.h"
+
+int main()
+{
+    /*
+    //Criar opções para compressão e descompressão?
+    char filename[500];
+    int options = 0;
+
+    while(options != 3)
+    {
+        printf("Escolha uma opção\n1- Compressão;\n2- Descompressão;\n3 - Sair");
+        scanf("%d", &options);
+        switch(options)
+        {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+                break;
+            default:
+                printf("Entrada inválida!\n");
+                break;
+        }
+    }
+    */
+   return 0;
+}
